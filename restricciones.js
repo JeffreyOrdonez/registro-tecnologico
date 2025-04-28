5df9e5d61e75e04945c6003a7f5de9fd99ceb1fb
JeffreyOrdoñez <jeffreyordonez7@gmail.com>
2025-04-28 04:47:16 -0600

    Finalizacion de la pagina de registro y restricciones javascript a los campos requeridos

--- restricciones.js
+++ restricciones.js
@@ -1,1 +1,38 @@
-fo
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("miFormulario");
+    const fullNameInput = document.getElementById("fullName");
+    const emailInput = document.getElementById("email");
+    const carreraSelect = document.getElementById("carrera");
+    const checkboxes = document.querySelectorAll('input[name="conferencias"]');
+
+    const maxFullNameLength = 50;
+    const maxEmailLength = 50;
+
+
+    fullNameInput.addEventListener("input", () => {
+        if (fullNameInput.value.length > maxFullNameLength) {
+            fullNameInput.value = fullNameInput.value.slice(0, maxFullNameLength);
+            alert(`El nombre completo no puede exceder los ${maxFullNameLength} caracteres.`);
+        }
+    });
+
+    emailInput.addEventListener("input", () => {
+        if (emailInput.value.length > maxEmailLength) {
+            emailInput.value = emailInput.value.slice(0, maxEmailLength);
+            alert(`El correo electrónico no puede exceder los ${maxEmailLength} caracteres.`);
+        }
+    });
+
+
+    emailInput.addEventListener("input", () => {
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@uamv\.edu\.ni$/;
+        if (!emailPattern.test(emailInput.value)) {
+            emailInput.setCustomValidity("El correo debe tener el formato ...@uamv.edu.ni");
+        } else {
+            emailInput.setCustomValidity("");
+        }
+        emailInput.reportValidity();
+    });
+
+
+});
